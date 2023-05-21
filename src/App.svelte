37a0b043622073493cca5894e8c40ee153147a15
Chris Wong <wc38.wong@gmail.com>
2023-05-21 15:19:37 +0800

    Poll Form Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from './components/Header.svelte';
   import Footer from './components/Footer.svelte';
+  import CreatePollForm from './components/CreatePollForm.svelte';
   import Tabs from './shared/Tabs.svelte';
 
   let items = ['Current Polls', 'Add New Poll'];
@@ -17,7 +18,7 @@
   {#if activeItem === items[0]}
     <p>Poll list component goes here</p>
   {:else if activeItem === items[1]}
-    <p>new poll component goes here</p>
+    <CreatePollForm />
   {/if}
 </main>
 <Footer/>
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,39 @@
+<script>
+  let fields = { question: '', answerA: '', answerB: ''};
+  const onSubmit = () => {
+    console.log(JSON.stringify(fields));
+  };
+</script>
+
+<form on:submit|preventDefault={onSubmit}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input type="text" id="question" bind:value={fields.question}/>
+  </div>
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input type="text" id="answer-a" bind:value={fields.answerA}/>
+  </div>
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input type="text" id="answer-b" bind:value={fields.answerB}/>
+  </div>
+  <button>Add Poll</button>
+</form>
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .form-field {
+    width: 100%;
+    border-radius: 6px;
+  }
+
+  label {
+    margin: 10px 5px;;
+    text-align: left;
+  }
+</style>
