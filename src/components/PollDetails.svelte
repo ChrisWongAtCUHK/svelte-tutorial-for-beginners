5741143e815a8e280c829430c9301e72d0929c64
Chris Wong <wc38.wong@gmail.com>
2023-05-25 07:06:18 +0800

    Poll Details Component

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,12 +1,19 @@
 <script>
   export let poll;
+  
+  $: totalVotes = poll.votesA + poll.votesB
 </script>
 
 <div class="poll">
   <h3>{poll.question}</h3>
+  <p>Total votes: {totalVotes}</p>
   <div class="answer">
     <div class="percent percent-a"></div>
     <span>{poll.answerA} ({poll.votesA})</span>
+  </div>
+  <div class="answer">
+    <div class="percent percent-b"></div>
+    <span>{poll.answerB} ({poll.votesB})</span>
   </div>
 </div>
 
