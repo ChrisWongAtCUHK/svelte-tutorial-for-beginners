b4660e7efead0ce12791d6380d477d277be7ed32
Chris Wong <wc38.wong@gmail.com>
2023-05-25 05:10:21 +0800

    Poll Details Component

---
+++ src/components/PollDetails.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let poll;
+</script>
+
+<div class="poll">
+  <h3>{poll.question}</h3>
+  <div class="answer">
+    <div class="percent percent-a"></div>
+    <span>{poll.answerA} ({poll.votesA})</span>
+  </div>
+</div>
+
+<style>
+</style>
